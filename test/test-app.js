eacf9351a2e3ae7658e8e761424121b1729c3063
mnadeem <coolmind182006@gmail.com>
2014-12-10 10:55:22 +0530

    cd

--- test/test-app.js
+++ test/test-app.js
@@ -7,22 +7,6 @@ var helpers = require('yeoman-generator').test;
 var os = require('os');
 
 describe('enterprise-angular-generator:app', function () {
-  before(function (done) {
-    helpers.run(path.join(__dirname, '../app'))
-      .inDir(path.join(os.tmpdir(), './temp-test'))
-      .withOptions({ 'skip-install': true })
-      .withPrompt({
-        someOption: true
-      })
-      .on('end', done);
-  });
+  
 
-  it('creates files', function () {
-    assert.file([
-      'bower.json',
-      'package.json',
-      '.editorconfig',
-      '.jshintrc'
-    ]);
-  });
 });
